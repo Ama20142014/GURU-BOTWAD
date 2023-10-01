869c841012d38a0535b017b9b762d916df8061fe
Ama20142014 <145349430+Ama20142014@users.noreply.github.com>
2023-10-01 11:21:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94703389681', 'me', true],
+  ['94703389681', 'King Bro', true], 
   [''] 
 ] //Number of owners
 
